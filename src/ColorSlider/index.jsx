698a602b7eb8c24b9869d174a155ca2cfb0215bc
marketamajerova <marketamajerova.6@gmail.com>
2023-03-21 22:06:42 +0100

    ColorSlider component done

--- src/ColorSlider/index.jsx
+++ src/ColorSlider/index.jsx
@@ -2,9 +2,18 @@ import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 
-const ColorSlider = () => {
+const ColorSlider = ({baseColor, colorName}) => {
     return (
         <>
+            <label htmlFor={baseColor}>{colorName}</label>
+                <input
+                    type="range"
+                    className={`slider slider--${baseColor}`}
+                    id={`${baseColor}Slider`}
+                    min="0"
+                    max="255"
+                    value="0"
+                />
         </>
     )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import ColorSlider from './ColorSlider';
 
 const App = () => {
+
+  const [redValue, setRedValue] = useState('');
+  const [greenValue, setGreenValue] = useState('');
+  const [blueValue, setBlueValue] = useState('');
+
+
   return (
     <body>
       <div class="color-panel">
         <h1>Mixér barev</h1>
         <div class="sliders">
-          <label for="red">Červená</label>
-          <input
-            type="range"
-            class="slider slider--red"
-            id="redSlider"
-            min="0"
-            max="255"
-            value="0"
-          />
+          <ColorSlider baseColor="red" colorName="Červená"/>
+          <ColorSlider baseColor="green" colorName="Zelená"/>
+          <ColorSlider baseColor="blue" colorName="Modrá"/>
 
-          <label for="green">Zelená</label>
+          {/* <label for="green">Zelená</label>
           <input
             type="range"
             class="slider slider--green"
@@ -36,9 +37,9 @@ const App = () => {
             min="0"
             max="255"
             value="0"
-          />
+          /> */}
         </div>
-        <div class="color-box" id="color-box"></div>
+        <div class="color-box" id="color-box" style={{backgroundColor: "blue"}}></div>
       </div>
     </body>
   );
