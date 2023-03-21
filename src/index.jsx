fe03cd5dcd73cf521ef2893650ee2a78434e27a4
marketamajerova <marketamajerova.6@gmail.com>
2023-03-21 22:37:44 +0100

    Setting 3 color states

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,9 @@ import ColorSlider from './ColorSlider';
 
 const App = () => {
 
-  const [redValue, setRedValue] = useState('');
-  const [greenValue, setGreenValue] = useState('');
-  const [blueValue, setBlueValue] = useState('');
+  const [redValue, setRedValue] = useState('55');
+  const [greenValue, setGreenValue] = useState('184');
+  const [blueValue, setBlueValue] = useState('195');
 
 
   return (
@@ -15,31 +15,14 @@ const App = () => {
       <div class="color-panel">
         <h1>Mixér barev</h1>
         <div class="sliders">
-          <ColorSlider baseColor="red" colorName="Červená"/>
-          <ColorSlider baseColor="green" colorName="Zelená"/>
-          <ColorSlider baseColor="blue" colorName="Modrá"/>
-
-          {/* <label for="green">Zelená</label>
-          <input
-            type="range"
-            class="slider slider--green"
-            id="greenSlider"
-            min="0"
-            max="255"
-            value="0"
-          />
-
-          <label for="blue">Modrá</label>
-          <input
-            type="range"
-            class="slider slider--blue"
-            id="blueSlider"
-            min="0"
-            max="255"
-            value="0"
-          /> */}
+          <ColorSlider baseColor="red" colorName="Červená" />
+          <ColorSlider baseColor="green" colorName="Zelená" />
+          <ColorSlider baseColor="blue" colorName="Modrá" />
         </div>
-        <div class="color-box" id="color-box" style={{backgroundColor: "blue"}}></div>
+        <div class="color-box"
+          id="color-box"
+          style={{backgroundColor: `rgb(${redValue}, ${greenValue}, ${blueValue})`}}>
+        </div>
       </div>
     </body>
   );
